a119a23d6e1c12c76c0854e8ccfbe8bd7886b8e9
CristianDavidVB <104736302+CristianDavidVB@users.noreply.github.com>
2022-09-30 22:32:04 -0500

    Se creo nueva ruta show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+    #Se hace el llamado de todos los datos de articles
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
